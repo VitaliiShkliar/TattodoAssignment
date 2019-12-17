5b723888c2b1a92e285aa20ac96090fb95c01d7b
Vitalii Shkliar <vshkliar@axondevgroup.com>
2019-12-17 10:39:17 +0200

    Initial Commit

---
+++ TatoodoAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TatoodoAssignment
+//
+//  Created by Vitalii Shkliar on 17.12.2019.
+//  Copyright © 2019 Vitalii Shkliar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
